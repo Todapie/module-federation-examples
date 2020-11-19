5f8c62db2aa5c40ab8b1889d0395830882100e4e
Josh Lucas <joshlucas@luma-m0002.lan>
2020-11-19 11:19:41 -0500

    set up skeleton apps

---
+++ angular-example/projects/app1/webpack.config.js
@@ -0,0 +1,20 @@
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+module.exports = {
+  output: {
+    publicPath: "http://localhost:4200/",
+    uniqueName: "app1",
+  },
+  optimization: {
+    runtimeChunk: false,
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      shared: {
+        "@angular/core": { eager: true, singleton: true },
+        "@angular/common": { eager: true, singleton: true },
+        "@angular/router": { eager: true, singleton: true },
+      },
+    }),
+  ],
+};
